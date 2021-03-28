ead97dbc9478fbcef05cce4559b887a5ce0ef60b
vietnguyen2000 <viet.nguyen.2000@hcmut.edu.vn>
2021-03-28 13:52:57 +0700

    [f] pause done

--- src/game/Ground/Ground.js
+++ src/game/Ground/Ground.js
@@ -6,13 +6,12 @@ import GroundController from "./GroundController.js";
 class Ground extends MyGameObject {
     constructor(pos = new Vec2(-300,-47)) {
         super(pos);
-        this.defaultPosition = pos;
-
         // Add sprite
         let img = new Image();
         img.src = 'art/Ground/Ground.png';
         this.sprite = new Sprite(this);
         this.sprite.setSprite(img);
+        console.log(this.sprite, this.position)
 
         //add physic
         this.physic = new Physic(this,0);
